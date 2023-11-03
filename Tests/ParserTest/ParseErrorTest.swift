52973205f85baaca5981007be128b54d5403bef5
kntkymt <kntkc0de@gmail.com>
2023-11-04 06:34:11 +0900

    Add tests for ParseError

---
+++ Tests/ParserTest/ParseErrorTest.swift
@@ -0,0 +1,49 @@
+import XCTest
+@testable import Parser
+import Tokenizer
+
+final class ParseErrorTest: XCTestCase {
+
+    func test2Operators() throws {
+        do {
+            _ = try parse(tokens: [
+                .number("1", sourceIndex: 0),
+                .reserved(.add, sourceIndex: 1)
+            ])
+        } catch let error as ParseError {
+            XCTAssertEqual(error, .invalidSyntax(index: 2))
+        }
+    }
+
+    func testInvalidPosition() throws {
+        do {
+            _ = try parse(tokens: [
+                .reserved(.mul, sourceIndex: 0),
+                .number("1", sourceIndex: 1),
+            ])
+        } catch let error as ParseError {
+            XCTAssertEqual(error, .invalidSyntax(index: 0))
+        }
+    }
+
+    func testInvalidPosition2() throws {
+        do {
+            _ = try parse(tokens: [
+                .number("1", sourceIndex: 0),
+                .reserved(.add, sourceIndex: 1),
+                .reserved(.mul, sourceIndex: 2),
+                .number("2", sourceIndex: 3),
+            ])
+        } catch let error as ParseError {
+            XCTAssertEqual(error, .invalidSyntax(index: 2))
+        }
+    }
+
+    func testEmpty() throws {
+        do {
+            _ = try parse(tokens: [])
+        } catch let error as ParseError {
+            XCTAssertEqual(error, .invalidSyntax(index: 0))
+        }
+    }
+}
