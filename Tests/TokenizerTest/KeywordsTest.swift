e217909a4814c5232e99d282f55a2e2163dc3cb0
kntkymt <kntkc0de@gmail.com>
2023-11-14 23:50:48 +0900

    Add if, else, while, for to Token

--- Tests/TokenizerTest/KeywordsTest.swift
+++ Tests/TokenizerTest/KeywordsTest.swift
@@ -54,4 +54,49 @@ final class KeywordsTest: XCTestCase {
             ]
         )
     }
+
+    func testIf() throws {
+        let tokens = try tokenize(source: "if(1)else 2")
+        XCTAssertEqual(
+            tokens,
+            [
+                .keyword(.if, sourceIndex: 0),
+                .reserved(.parenthesisLeft, sourceIndex: 2),
+                .number("1", sourceIndex: 3),
+                .reserved(.parenthesisRight, sourceIndex: 4),
+                .keyword(.else, sourceIndex: 5),
+                .number("2", sourceIndex: 10)
+            ]
+        )
+    }
+
+    func testWhile() throws {
+        let tokens = try tokenize(source: "while(1)")
+        XCTAssertEqual(
+            tokens,
+            [
+                .keyword(.while, sourceIndex: 0),
+                .reserved(.parenthesisLeft, sourceIndex: 5),
+                .number("1", sourceIndex: 6),
+                .reserved(.parenthesisRight, sourceIndex: 7),
+            ]
+        )
+    }
+
+    func testFor() throws {
+        let tokens = try tokenize(source: "for(1;2;3)")
+        XCTAssertEqual(
+            tokens,
+            [
+                .keyword(.for, sourceIndex: 0),
+                .reserved(.parenthesisLeft, sourceIndex: 3),
+                .number("1", sourceIndex: 4),
+                .reserved(.semicolon, sourceIndex: 5),
+                .number("2", sourceIndex: 6),
+                .reserved(.semicolon, sourceIndex: 7),
+                .number("3", sourceIndex: 8),
+                .reserved(.parenthesisRight, sourceIndex: 9),
+            ]
+        )
+    }
 }
