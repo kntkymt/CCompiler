15299a25b6e619a132958297e05476dfe47d040e
kntkymt <kntkc0de@gmail.com>
2023-11-18 23:30:41 +0900

    Fix structure of calling tokenize, parse, gen

--- Sources/CCompilerCore/CCompilerCore.swift
+++ Sources/CCompilerCore/CCompilerCore.swift
@@ -9,35 +9,21 @@ public enum CompileError: Error, Equatable {
 }
 
 public func compile(_ source: String) throws -> String {
-    let tokens = try {
-        do {
-            return try tokenize(source: source)
-        } catch let error as TokenizeError {
-            switch error {
-            case .unknownToken(let index):
-                throw CompileError.invalidToken(index: index)
-            }
-
-        } catch {
-            throw CompileError.unknown
-        }
-    }()
-    let node = try {
-        do {
-            return try Parser(tokens: tokens).parse()
-        } catch let error as ParseError {
-            switch error {
-            case .invalidSyntax(let index):
-                throw CompileError.invalidSyntax(index: index)
-            }
-
-        } catch {
-            throw CompileError.unknown
-        }
-    }()
-
     do {
+        let tokens = try tokenize(source: source)
+        let node = try Parser(tokens: tokens).parse()
+
         return try Generator().generate(sourceFileNode: node)
+    } catch let error as TokenizeError {
+        switch error {
+        case .unknownToken(let index):
+            throw CompileError.invalidToken(index: index)
+        }
+    } catch let error as ParseError {
+        switch error {
+        case .invalidSyntax(let index):
+            throw CompileError.invalidSyntax(index: index)
+        }
     } catch let error as GenerateError {
         switch error {
         case .invalidSyntax(let index):
