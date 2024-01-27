ae717e979395f9d767c3b1f2477b2859e2d64f53
kntkymt <kntkc0de@gmail.com>
2024-01-28 04:47:27 +0900

    Improve test on ~=

--- Tests/TokenizerTest/TokenSpecTest.swift
+++ Tests/TokenizerTest/TokenSpecTest.swift
@@ -7,6 +7,13 @@ final class TokenSpecTest: XCTestCase {
             let spec = TokenSpec.reserved(kind)
             let token = Token(kind: .reserved(kind), sourceRange: SourceRange(start: .startOfFile, end: .startOfFile))
             XCTAssert(spec ~= token)
+
+            XCTAssertFalse(TokenSpec.keyword(.if) ~= token)
+            XCTAssertFalse(TokenSpec.integerLiteral ~= token)
+            XCTAssertFalse(TokenSpec.stringLiteral ~= token)
+            XCTAssertFalse(TokenSpec.identifier ~= token)
+            XCTAssertFalse(TokenSpec.type ~= token)
+            XCTAssertFalse(TokenSpec.endOfFile ~= token)
         }
     }
 
@@ -15,6 +22,13 @@ final class TokenSpecTest: XCTestCase {
             let spec = TokenSpec.keyword(kind)
             let token = Token(kind: .keyword(kind), sourceRange: SourceRange(start: .startOfFile, end: .startOfFile))
             XCTAssert(spec ~= token)
+
+            XCTAssertFalse(TokenSpec.reserved(.add) ~= token)
+            XCTAssertFalse(TokenSpec.integerLiteral ~= token)
+            XCTAssertFalse(TokenSpec.stringLiteral ~= token)
+            XCTAssertFalse(TokenSpec.identifier ~= token)
+            XCTAssertFalse(TokenSpec.type ~= token)
+            XCTAssertFalse(TokenSpec.endOfFile ~= token)
         }
     }
 
@@ -22,18 +36,39 @@ final class TokenSpecTest: XCTestCase {
         let spec = TokenSpec.integerLiteral
         let token = Token(kind: .integerLiteral("1"), sourceRange: SourceRange(start: .startOfFile, end: .startOfFile))
         XCTAssert(spec ~= token)
+
+        XCTAssertFalse(TokenSpec.reserved(.add) ~= token)
+        XCTAssertFalse(TokenSpec.keyword(.if) ~= token)
+        XCTAssertFalse(TokenSpec.stringLiteral ~= token)
+        XCTAssertFalse(TokenSpec.identifier ~= token)
+        XCTAssertFalse(TokenSpec.type ~= token)
+        XCTAssertFalse(TokenSpec.endOfFile ~= token)
     }
 
     func testStringLiteral() {
         let spec = TokenSpec.stringLiteral
         let token = Token(kind: .stringLiteral("a"), sourceRange: SourceRange(start: .startOfFile, end: .startOfFile))
         XCTAssert(spec ~= token)
+
+        XCTAssertFalse(TokenSpec.reserved(.add) ~= token)
+        XCTAssertFalse(TokenSpec.keyword(.if) ~= token)
+        XCTAssertFalse(TokenSpec.integerLiteral ~= token)
+        XCTAssertFalse(TokenSpec.identifier ~= token)
+        XCTAssertFalse(TokenSpec.type ~= token)
+        XCTAssertFalse(TokenSpec.endOfFile ~= token)
     }
 
     func testIdentifier() {
         let spec = TokenSpec.identifier
         let token = Token(kind: .identifier("a"), sourceRange: SourceRange(start: .startOfFile, end: .startOfFile))
         XCTAssert(spec ~= token)
+
+        XCTAssertFalse(TokenSpec.reserved(.add) ~= token)
+        XCTAssertFalse(TokenSpec.keyword(.if) ~= token)
+        XCTAssertFalse(TokenSpec.integerLiteral ~= token)
+        XCTAssertFalse(TokenSpec.stringLiteral ~= token)
+        XCTAssertFalse(TokenSpec.type ~= token)
+        XCTAssertFalse(TokenSpec.endOfFile ~= token)
     }
 
     func testType() {
@@ -41,6 +76,12 @@ final class TokenSpecTest: XCTestCase {
             let spec = TokenSpec.type
             let token = Token(kind: .type(kind), sourceRange: SourceRange(start: .startOfFile, end: .startOfFile))
             XCTAssert(spec ~= token)
+
+            XCTAssertFalse(TokenSpec.reserved(.add) ~= token)
+            XCTAssertFalse(TokenSpec.keyword(.if) ~= token)
+            XCTAssertFalse(TokenSpec.integerLiteral ~= token)
+            XCTAssertFalse(TokenSpec.stringLiteral ~= token)
+            XCTAssertFalse(TokenSpec.endOfFile ~= token)
         }
     }
 
@@ -48,5 +89,11 @@ final class TokenSpecTest: XCTestCase {
         let spec = TokenSpec.endOfFile
         let token = Token(kind: .endOfFile, sourceRange: SourceRange(start: .startOfFile, end: .startOfFile))
         XCTAssert(spec ~= token)
+
+        XCTAssertFalse(TokenSpec.reserved(.add) ~= token)
+        XCTAssertFalse(TokenSpec.keyword(.if) ~= token)
+        XCTAssertFalse(TokenSpec.integerLiteral ~= token)
+        XCTAssertFalse(TokenSpec.stringLiteral ~= token)
+        XCTAssertFalse(TokenSpec.type ~= token)
     }
 }
