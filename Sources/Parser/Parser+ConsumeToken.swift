ddbe5e12412d6710417c938f0e9c97ff9a748fcc
kntkymt <kntkc0de@gmail.com>
2024-01-28 02:55:46 +0900

    Add variadic parameters consume, at

--- Sources/Parser/Parser+ConsumeToken.swift
+++ Sources/Parser/Parser+ConsumeToken.swift
@@ -5,8 +5,26 @@ extension Parser {
         return spec ~= tokens[index]
     }
 
+    func at(_ specs: TokenSpec...) -> Bool {
+        for spec in specs where at(spec) {
+            return true
+        }
+
+        return false
+    }
+
     func consume(if spec: TokenSpec) -> TokenNode? {
         return try? consume(spec)
+    }
+
+    func consume(if specs: TokenSpec...) -> TokenNode? {
+        for spec in specs {
+            if let token = consume(if: spec) {
+                return token
+            }
+        }
+
+        return nil
     }
 
     /// `eat` in SwiftSyntax
@@ -18,5 +36,15 @@ extension Parser {
         } else {
             throw ParseError.invalidSyntax(location: tokens[index].sourceRange.start)
         }
+    }
+
+    func consume(_ specs: TokenSpec...) throws -> TokenNode {
+        for spec in specs where at(spec) {
+            let token = tokens[index]
+            index += 1
+            return TokenNode(token: token)
+        }
+
+        throw ParseError.invalidSyntax(location: tokens[index].sourceRange.start)
     }
 }
