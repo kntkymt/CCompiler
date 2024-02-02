f852791eee3adabd2cdbcb06fd5d3c42b60013d5
kntkymt <kntkc0de@gmail.com>
2024-02-02 22:40:00 +0900

    Remove functionLabel from inst on AsmPrinter

--- Sources/Generator/ArmAsmPrinter.swift
+++ Sources/Generator/ArmAsmPrinter.swift
@@ -13,101 +13,114 @@ public enum ArmAsmPrinter {
     }
 
     static func print(instruction: AsmRepresent.Instruction) -> String {
-        func inst(opecode: String, a: AsmRepresent.Register, b: AsmRepresent.Register) -> String {
+        /// opecode a, b
+        func inst(_ opecode: String, _ a: AsmRepresent.Register, _ b: AsmRepresent.Register) -> String {
             "    \(opecode) \(a.rawValue), \(b.rawValue)\n"
         }
 
-        func inst(opecode: String, a: AsmRepresent.Register, immediate: String) -> String {
+        /// opecode a, #immediate
+        func inst(_ opecode: String, _ a: AsmRepresent.Register, _ immediate: Int64) -> String {
             "    \(opecode) \(a.rawValue), #\(immediate)\n"
         }
 
-        func inst(opecode: String, a: AsmRepresent.Register, b: AsmRepresent.Register, c: AsmRepresent.Register) -> String {
+        /// opecode a, b, c
+        func inst(_ opecode: String, _ a: AsmRepresent.Register, _ b: AsmRepresent.Register, _ c: AsmRepresent.Register) -> String {
             "    \(opecode) \(a.rawValue), \(b.rawValue), \(c.rawValue)\n"
         }
 
-        func inst(opecode: String, a: AsmRepresent.Register, b: AsmRepresent.Register, immediate: String) -> String {
+        /// opecode a, b, #immediate
+        func inst(_ opecode: String, _ a: AsmRepresent.Register, _ b: AsmRepresent.Register, _ immediate: Int64) -> String {
             "    \(opecode) \(a.rawValue), \(b.rawValue), #\(immediate)\n"
         }
 
-        func inst(opecode: String, a: AsmRepresent.Register, address: AsmRepresent.Instruction.Address) -> String {
+        /// opecode a, [address]
+        func inst(_ opecode: String, _ a: AsmRepresent.Register, _ address: AsmRepresent.Instruction.Address) -> String {
             "    \(opecode) \(a.rawValue), [\(address.description)]\n"
         }
 
-        func inst(opecode: String, a: AsmRepresent.Register, b: AsmRepresent.Register, address: AsmRepresent.Instruction.Address) -> String {
+        /// opecode a, b, [address]
+        func inst(_ opecode: String, _ a: AsmRepresent.Register, _ b: AsmRepresent.Register, _ address: AsmRepresent.Instruction.Address) -> String {
             "    \(opecode) \(a.rawValue), \(b.rawValue), [\(address.description)]\n"
         }
 
-        func inst(opecode: String, label: String) -> String {
-            "    \(opecode) \(label)\n"
+        /// opecode label
+        func inst(_ opecode: String, _ label: String) -> String {
+            "    \(opecode) \(fixLabel(label))\n"
+        }
+
+        /// AppleClangではmainが_main じゃないとダメなので対応
+        func fixLabel(_ label: String) -> String {
+            label == "main" ? "_main" : label
         }
 
         return switch instruction {
         case .mov(let dst, let src): 
-            inst(opecode: "mov", a: dst, b: src)
+            inst("mov", dst, src)
         case .movi(let dst, let immediate): 
-            inst(opecode: "mov", a: dst, immediate: immediate.description)
+            inst("mov", dst, immediate)
         case .add(let dst, let src1, let src2): 
-            inst(opecode: "add", a: dst, b: src1, c: src2)
+            inst("add", dst, src1, src2)
         case .addi(let dst, let src, let immediate):
-            inst(opecode: "add", a: dst, b: src, immediate: immediate.description)
+            inst("add", dst, src, immediate)
         case .sub(let dst, let src1, let src2):
-            inst(opecode: "sub", a: dst, b: src1, c: src2)
+            inst("sub", dst, src1, src2)
         case .subi(let dst, let src, let immediate):
-            inst(opecode: "sub", a: dst, b: src, immediate: immediate.description)
+            inst("sub", dst, src, immediate)
         case .mul(let dst, let src1, let src2):
-            inst(opecode: "mul", a: dst, b: src1, c: src2)
+            inst("mul", dst, src1, src2)
         case .muli(let dst, let src, let immediate):
-            inst(opecode: "mul", a: dst, b: src, immediate: immediate.description)
+            inst("mul", dst, src, immediate)
         case .div(let dst, let src1, let src2):
-            inst(opecode: "sdiv", a: dst, b: src1, c: src2)
+            inst("sdiv", dst, src1, src2)
         case .divi(let dst, let src, let immediate):
-            inst(opecode: "sdiv", a: dst, b: src, immediate: immediate.description)
+            inst("sdiv", dst, src, immediate)
         case .push(let src):
-            "    str \(src.rawValue), [sp, #-16]!\n"
+            inst("sub", .sp, .sp, 16)
+            + inst("str", src, .register(.sp))
         case .pop(let dst):
-            inst(opecode: "ldr", a: dst, address: .register(.sp))
-            + inst(opecode: "add", a: .sp, b: .sp, immediate: "16")
+            inst("ldr", dst, .register(.sp))
+            + inst("add", .sp, .sp, 16)
         case .addr(let dst, let label):
             "    adrp \(dst.rawValue), \(label)@GOTPAGE\n"
             + "    ldr \(dst.rawValue), [\(dst.rawValue), \(label)@GOTPAGEOFF]\n"
         case .str(let src, let address):
-            inst(opecode: "str", a: src, address: address)
+            inst("str", src, address)
         case .strb(let src, let address):
-            inst(opecode: "strb", a: src, address: address)
+            inst("strb", src, address)
         case .stp(let src1, let src2, let address):
-            inst(opecode: "stp", a: src1, b: src2, address: address)
+            inst("stp", src1, src2, address)
         case .ldr(let dst, let address):
-            inst(opecode: "ldr", a: dst, address: address)
+            inst("ldr", dst, address)
         case .ldrb(let dst, let address):
-            inst(opecode: "ldrb", a: dst, address: address)
+            inst("ldrb", dst, address)
         case .ldp(let dst1, let dst2, let address):
-            inst(opecode: "ldp", a: dst1, b: dst2, address: address)
+            inst("ldp", dst1, dst2, address)
         case .cmp(let src1, let src2):
-            inst(opecode: "cmp", a: src1, b: src2)
+            inst("cmp", src1, src2)
         case .cmpi(let src, let immediate):
-            inst(opecode: "cmp", a: src, immediate: immediate.description)
+            inst("cmp", src, immediate)
         case .cset(let dst, let flag):
             "    cset \(dst.rawValue), \(flag.rawValue)\n"
         case .beq(let label):
-            inst(opecode: "beq", label: label == "main" ? "_main" : label)
+            inst("beq", label)
         case .b(let label):
-            inst(opecode: "b", label: label == "main" ? "_main" : label)
+            inst("b", label)
         case .bl(let label):
-            inst(opecode: "bl", label: label == "main" ? "_main" : label)
+            inst("bl", label)
         case .neg(let dst, let src):
-            inst(opecode: "neg", a: dst, b: src)
+            inst("neg", dst, src)
         case .lsli(let dst, let src, let immediate):
-            inst(opecode: "lsl", a: dst, b: src, immediate: immediate.description)
+            inst("lsl", dst, src, Int64(immediate))
         case .ret:
             "    ret\n"
         case .label(let label):
-            "\(label == "main" ? "_main" : label):\n"
+            "\(fixLabel(label)):\n"
         case .p2align(let factor):
             ".p2align \(factor)\n"
         case .section(let kinds):
             ".section \(kinds.map { $0.rawValue }.joined(separator: ","))\n"
         case .globl(let label):
-            ".globl \(label == "main" ? "_main" : label)\n"
+            ".globl \(fixLabel(label))\n"
         case .dataDecl(let kind, let value):
             "    .\(kind.rawValue) \(value)\n"
         }
